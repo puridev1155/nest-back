f91d0517ae203b77933a8e94e45545d772d1774f
puridev1155 <92970876+puridev1155@users.noreply.github.com>
2025-05-18 23:29:38 +0900

    추가 업데이트

--- ExpressStart/dist/app.model.d.ts
+++
@@ -1,9 +0,0 @@
-export type CatType = {
-    id: string;
-    name: string;
-    age: number;
-    species: string;
-    isCute: boolean;
-    friends: string[];
-};
-export declare const Cat: CatType[];
